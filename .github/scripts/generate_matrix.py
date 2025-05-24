cdbe516d3aff21afcdfbfeeed4ce83cfa4c784c1
shaahji <hiteshshah@microsoft.com>
2025-05-24 01:14:21 -0700

    Use GITHUB_OUTPUT

--- .github/scripts/generate_matrix.py
+++ .github/scripts/generate_matrix.py
@@ -26,4 +26,5 @@ output = json.dumps(matrix)
 # print("shlex.quote", shlex.quote(output))   # '{"include": [{"name": "bert/ptq_cpu", "config": "bert\\olive_ci_ptq_cpu.json"}, {"name": "phi3_5/qnn", "config": "phi3_5\\olive_ci_qnn.json"}]}'
 # print("json.dumps", json.dumps(output))     # {\"include\": [{\"name\": \"bert/ptq_cpu\", \"config\": \"bert\\\\olive_ci_ptq_cpu.json\"}, {\"name\": \"phi3_5/qnn\", \"config\": \"phi3_5\\\\olive_ci_qnn.json\"}]}
 output = json.dumps(output)[1:-1]
-print(f"::set-output name=matrix::{output}")
+#print(f"::set-output name=matrix::{output}")
+print(output)
